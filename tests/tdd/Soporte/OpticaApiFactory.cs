1a6cc3d92ba4e88e49001ad5926c3794da9c83ad
Diego Castro Satt <diegocastrosatt@gmail.com>
2026-09-08 16:33:35 -0300

    correccion de un leve error

--- tests/tdd/Soporte/OpticaApiFactory.cs
+++ tests/tdd/Soporte/OpticaApiFactory.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Optica.Api.Data;
+using Xunit;
 
 namespace Optica.Api.Tdd.Tests.Soporte;
 
