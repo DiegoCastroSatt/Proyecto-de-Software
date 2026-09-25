7f789ec263731b607d3531e4cc29eb4ac8613d7c
Diego Castro Satt <diegocastrosatt@gmail.com>
2026-09-25 14:58:12 -0300

    id de producto ahora puede ser null

--- Optica.Api/Modules/Ventas/DTOs/VentaResponseDto.cs
+++ Optica.Api/Modules/Ventas/DTOs/VentaResponseDto.cs
@@ -1,7 +1,7 @@
 namespace Optica.Api.Modules.Ventas.DTOs;
 
-public record DetalleVentaResponseDto(int ProductoId, int Cantidad, decimal PrecioUnitario, decimal Subtotal);
-public record ProductoHistorialDto(int ProductoId, string? Nombre, int Cantidad, decimal PrecioUnitario, decimal Subtotal);
+public record DetalleVentaResponseDto(int? ProductoId, int Cantidad, decimal PrecioUnitario, decimal Subtotal);
+public record ProductoHistorialDto(int? ProductoId, string? Nombre, int Cantidad, decimal PrecioUnitario, decimal Subtotal);
 public record VentaResponseDto(int IdVenta, DateTime Fecha, decimal Total, IReadOnlyList<ProductoHistorialDto> Productos);
 public record VentaCreadaResponseDto(int IdVenta, DateTime Fecha, decimal Total, IReadOnlyList<DetalleVentaResponseDto> Productos);
 public record ProductoCajaDto(int IdProducto, string CodigoProducto, string Nombre, decimal Precio);
